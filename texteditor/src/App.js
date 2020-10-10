09aa0157f1f36c943ee534c872e19cb08ff8103a
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-10 10:42:16 -0700

    Added basic text editor

--- texteditor/src/App.js
+++ texteditor/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TextEditor from "./components/TextEditor";
 
 function App() {
+  const [text, setText] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TextEditor
+        text={text}
+        setText={setText}
+        language={language}
+        setLanguage={setLanguage}
+      />
+    </>
   );
 }
 
---
+++ texteditor/src/components/TextEditor.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/javascript/javascript";
+import { Controlled as CodeMirror } from "react-codemirror2";
+
+export default function TextEditor(props) {
+  const { text, setText, language, setLanguage } = props;
+
+  function handleTextEditorChange(editor, data, value) {
+    setText(value);
+  }
+
+  return (
+    <>
+      <CodeMirror
+        value={text}
+        onBeforeChange={handleTextEditorChange}
+        options={{
+          lineWrapping: true,
+          lineNumbers: true,
+          lint: true,
+          mode: { language },
+          theme: "material",
+        }}
+      />
+    </>
+  );
+}
