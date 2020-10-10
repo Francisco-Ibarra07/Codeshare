3fe6233c5e77c1873fb54f8334dec8f27e2d3016
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-10 12:00:23 -0700

    Created server.js

---
+++ texteditor/src/server.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const socketio = require("socket.io");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = socketio(server);
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // Listen for 'text change' messages and forward
+  // them to everyone else
+  socket.on("text change", (newText) => {
+    socket.broadcast.emit("text change", newText);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
