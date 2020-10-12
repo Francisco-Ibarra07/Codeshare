8d2b6c6e8ec2bb4cdfcc52584c2dea09e95d9d9f
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-12 00:13:40 -0700

    Added routing to App.js

--- texteditor/src/App.js
+++ texteditor/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Room from "./pages/Room";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Room />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route component={HomePage} exact path="/" />
+        <Route component={Room} exact path="/:roomName" />
+      </Switch>
+    </BrowserRouter>
   );
 }
