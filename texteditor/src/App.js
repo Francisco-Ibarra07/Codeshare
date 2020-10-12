315d1582c6920f69964df463626e0ae4ad77a01f
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-12 02:33:55 -0700

    Added lazy loading to App.js

--- texteditor/src/App.js
+++ texteditor/src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import RoomPage from "./pages/RoomPage";
-import HomePage from "./pages/HomePage";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const RoomPage = lazy(() => import("./pages/RoomPage"));
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route component={HomePage} exact path="/" />
-        <Route component={RoomPage} exact path="/room/:roomName" />
-      </Switch>
-    </BrowserRouter>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <Switch>
+          <Route component={HomePage} exact path="/" />
+          <Route component={RoomPage} exact path="/room/:roomName" />
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
   );
 }
