ab95e410775ae0d53b6a733de8e6b73932de6fe2
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-11 19:11:12 -0700

    Implemented the dropdown component from react bootstrap

--- texteditor/src/components/TextEditor.js
+++ texteditor/src/components/TextEditor.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "codemirror/mode/php/php";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/python/python";
+import "codemirror/mode/javascript/javascript";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
-import "codemirror/mode/javascript/javascript";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import { Controlled as CodeMirror } from "react-codemirror2";
+import { supportedLanguages, getLanguageMode } from "../constants/editor";
 
 export default function TextEditor(props) {
   const { text, setText, language, setLanguage } = props;
@@ -11,13 +18,27 @@ export default function TextEditor(props) {
     setText(value);
   }
 
+  function handleLanguageChange(newLang) {
+    setLanguage(newLang);
+  }
+
   return (
     <>
       <div className="editor-container">
         <div className="editor-top">
-          Display Name: {language}
-          <button>Change language</button>
+          Display Name: John Smith
           <button>Generate Invitiation Link </button>
+          <DropdownButton title={language}>
+            {supportedLanguages.map((lang) => (
+              <Dropdown.Item
+                key={`dropdown-item-${lang}`}
+                onSelect={handleLanguageChange}
+                eventKey={lang}
+              >
+                {lang}
+              </Dropdown.Item>
+            ))}
+          </DropdownButton>
         </div>
 
         <CodeMirror
@@ -28,7 +49,7 @@ export default function TextEditor(props) {
             lineWrapping: true,
             lineNumbers: true,
             lint: true,
-            mode: language,
+            mode: getLanguageMode(language),
             theme: "material",
           }}
         />
