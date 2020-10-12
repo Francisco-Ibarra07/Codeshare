7329f7f5004bf3d748a2edc9e3d957728a345f28
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2020-10-12 02:26:00 -0700

    Created route to create room and check if exists

--- texteditor/server.js
+++ texteditor/server.js
@@ -5,17 +5,55 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = socketio(server);
+const cors = require("cors");
 const port = 5000;
 
+let rooms = {
+  defaultRoom: {
+    users: [],
+  },
+};
+
 // app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
 
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname, "build", "index.html"));
   res.send("Hello World");
 });
 
+// Creates new room
+app.post("/new", (req, res) => {
+  const { roomName } = req.body;
+
+  if (roomName === undefined) {
+    return res.sendStatus(400);
+  }
+
+  rooms[roomName] = { users: [] };
+  return res.sendStatus(201);
+});
+
+// Simply checks if a room with that name exists
+app.get("/exists/:roomName", (req, res) => {
+  const { roomName } = req.params;
+  if (rooms[roomName] === undefined) {
+    return res.sendStatus(404);
+  } else {
+    return res.sendStatus(200);
+  }
+});
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  const roomName = socket.handshake.query.roomName;
+  if (roomName === undefined || rooms[roomName] === undefined) {
+    console.error("Room name not specified or DNE");
+    socket.disconnect();
+    return;
+  }
+
+  // Todo: join user to roomName
+  console.log(`User connecting to room '${roomName}'`);
 
   // Listen for 'text change' messages and forward
   // them to everyone else
