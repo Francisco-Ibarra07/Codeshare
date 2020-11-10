8bae2664f2d1df230da23302dca9e0378ff4520c
Nathan <abegaznathan@gmail.com>
2020-11-09 19:22:09 -0800

    Initial Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <Link
-      to={{
-        pathname: "/room/defaultRoom",
-        state: { displayName: "francisco" },
-      }}
-    >
-      Click here to go to /room/defaultRoom
-    </Link>
+    <>
+    
+      <div className="homepage_container">
+        <form className="box" action="">
+          <h1>CodeShare</h1>
+          <h3>Realtime Collaborative Text Editor and Whiteboard</h3>
+          <input type="text" placeholder="Enter Name" />
+          <input type="text" placeholder="Enter Room Name" />
+          <button type="submit">Begin Session</button>
+          <br />
+          <Link
+            to={{
+              pathname: "/room/defaultRoom",
+              state: { displayName: "francisco" },
+            }}
+          >
+            Click here to go to /room/defaultRoom
+          </Link>
+        </form>
+      </div>
+    </>
   );
 }
